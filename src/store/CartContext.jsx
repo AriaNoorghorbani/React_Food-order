71605a9f5c9e5ce1b633425c1b7d0d3cf9e56961
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-07 10:56:30 +0300

    implement remove item

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,3 +1,4 @@
+import e from "cors";
 import { useReducer } from "react";
 import { createContext } from "react";
 
@@ -9,26 +10,44 @@ const cartContext = createContext({
 
 function cartReducer(state, action) {
   if (action === "ADD_ITEM") {
-    const existingIndexItem = state.items.findIndex(
+    const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
 
     const updatedItems = [...state.items];
 
-    if (existingIndexItem > -1) {
-      const existingItem = state.items[existingIndexItem];
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
       const updatedItem = {
         ...existingItem,
         quantity: existingItem.quantity + 1,
       };
-      updatedItems[existingIndexItem] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems.push({ ...action.item, quantity: 1 });
     }
+    return { ...state, items: updatedItems };
   }
 
   if (action === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.items.id
+    );
+
+    const updatedItems = [...state.items];
+    if (existingCartItemIndex === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
+
   return state;
 }
 
