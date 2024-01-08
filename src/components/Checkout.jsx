3fa8ceb8e3e455fdb0b28039457f926be4248053
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-08 10:30:08 +0300

    Implement form data

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -18,9 +18,17 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    console.log(customerData);
+  }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount:{currencyFormatter.format(totalPrice)}</p>
         <Input id="full-name" label="Full Name"></Input>
