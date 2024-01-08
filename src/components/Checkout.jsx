226d30016cde73f25f18104d0e7f8ff095b3a805
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-08 10:47:42 +0300

    Implement post order

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -23,7 +23,19 @@ export default function Checkout() {
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
-    console.log(customerData);
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -31,7 +43,7 @@ export default function Checkout() {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount:{currencyFormatter.format(totalPrice)}</p>
-        <Input id="full-name" label="Full Name"></Input>
+        <Input id="name" label="Full Name"></Input>
         <Input id="email" label="E-Mail"></Input>
         <Input id="street" label="Street"></Input>
         <div className="control-row">
